bda510fc0d197c46b35a18a2548a43b798032869
bartek-adamczewski <bartosz.adamczewski1@wp.pl>
2021-06-08 21:32:31 +0200

    created file for zad 1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,4 @@
 fun main() {
-    println("Podaj pierwszą liczbę: ")
-    val firstNumber = readLine()!!.toFloat()
-    println("Podaj drugą liczbę: ")
-    val secondNumber = readLine()!!.toFloat()
-    println("Podaj operację którą chcesz na nich wykonać: ")
-    val operation = readLine()!!
-
-    when {
-        operation == "/" && secondNumber != 0f -> println(firstNumber / secondNumber)
-        operation == "/" && secondNumber == 0f -> println("Pamiętaj cholero, nie dziel przez zero")
-        operation == "*" || operation == "x" -> println(firstNumber * secondNumber)
-        operation == "+" -> println(firstNumber + secondNumber)
-        operation == "-" -> println(firstNumber - secondNumber)
-        else -> println("nie znane działanie matematyczne")
-    }
-
+   zad1()
 }
 
